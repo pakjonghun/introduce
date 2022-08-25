9589f9d98a6305e4e74d6e4c2974c6948f10193d
정훈박 <fireking5997@gmail.com>
2022-08-25 17:09:16 +0900

    fix:테스트옵저버 추가, reset 스타일 수정

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -1,28 +1,16 @@
-import { media } from "./media";
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
 
 *,*::after,*::before{
+  margin:0;
+  padding:0;
+  outline: none;
   box-sizing:inherit ;
-  margin:0;padding:0;
 }
 
 html{
   font-size:62.5% ;
-  ${media.lg}{
-    font-size:55%;
-  }
-  ${media.md}{
-    font-size:50%;
-  }
-  ${media.sm}{
-    font-size:45%;
-  }
-  ${media.xs}{
-    font-size:43%;
-  }
-
 }
 
 body{
@@ -35,17 +23,6 @@ body{
   font-size:${({ theme }) => theme.sizes.defaultFont} ;
   font-family:${({ theme }) => theme.fonts.primaryEnFont} ;
   color:${({ theme }) => theme.colors.grayDark1};
-    background-image: linear-gradient(
-    to bottom right,
-    ${({ theme }) => theme.colors.primaryLight},
-    ${({ theme }) => theme.colors.primary}
-  );
-
-  ${media.xs}{
-    width:100%;
-    background-image:none ;
-    background-color:${({ theme }) => theme.colors.primary} ;
-  }
 }
 
 input{
